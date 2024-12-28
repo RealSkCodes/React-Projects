7d5fc70aeef30771d2a05b95ea52a5d66b1b3ba7
RealSkCodes <codingwithrealsk@gmail.com>
2024-12-28 14:01:34 +0530

    fetch fix

--- eat-more/src/utils/useFetchedData.js
+++ eat-more/src/utils/useFetchedData.js
@@ -4,16 +4,34 @@ const useFetchedData = (apiLink) => {
   const [data, setData] = useState([])
 
   const fetchData = async () => {
-    const data = await fetch(apiLink)
-    const json = await data.json()
-    setData(json)
-    console.count("Api called")
+    try {
+      const response = await fetch(apiLink, {
+        method: "GET", // Using GET directly
+        headers: {
+          "Content-Type": "application/json",
+          Origin: "https://your-vercel-domain.vercel.app", // Adjust the origin as needed
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      })
+
+      // Check if the response is successful
+      if (!response.ok) {
+        throw new Error("Failed to fetch data")
+      }
+
+      const json = await response.json()
+      setData(json)
+      console.count("API called")
+    } catch (error) {
+      console.error("Error fetching data:", error)
+    }
   }
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [apiLink])
 
   return data
 }
+
 export default useFetchedData
