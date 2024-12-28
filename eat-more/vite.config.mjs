97e97692553f2a75700b34b6bc0e88643ea5bcd3
RealSkCodes <codingwithrealsk@gmail.com>
2024-12-28 15:33:51 +0530

    rollbacked stuff

--- eat-more/vite.config.mjs
+++ eat-more/vite.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// Vite config to handle proxying API calls in development
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.swiggy.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-      },
-    },
-  },
 })
