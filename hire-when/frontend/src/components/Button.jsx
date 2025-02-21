82689c6951fafae582021409d3a6f2b5fbb68f78
RealSkCodes <codingwithrealsk@gmail.com>
2025-02-21 10:37:53 +0530

    refactor: modify Button.jsx and InputField.jsx, add autofill-text-input class to style autofill input

--- hire-when/frontend/src/components/Button.jsx
+++ hire-when/frontend/src/components/Button.jsx
@@ -1,14 +1,18 @@
 import { twMerge } from "tailwind-merge"
 
-const Button = ({ children, className, ...props }) => {
-  return (
-    <button
-      className={twMerge("flex items-center rounded-md text-black bg-gray-400", className)}
-      {...props}
-    >
-      {children}
-    </button>
-  )
+const Button = ({ children, className, type, ...props }) => {
+	return (
+		<button
+			type={type}
+			className={twMerge(
+				"flex items-center rounded-md text-black bg-gray-400",
+				className
+			)}
+			{...props}
+		>
+			{children}
+		</button>
+	)
 }
 
 export default Button
--- hire-when/frontend/src/components/InputField.jsx
+++ hire-when/frontend/src/components/InputField.jsx
@@ -1,31 +1,52 @@
+import { twMerge } from "tailwind-merge"
+
 const InputField = ({
-  mainBgStyle = "",
-  title = "",
-  titleStyle = "",
-  inpType = "text",
-  inpPlaceholder = "Type here....",
-  inpValue,
-  onInpChange,
-  inpStyle = "",
-  isRequired = false,
-  min = "",
-  max = "",
+	mainBgStyle = "",
+	title = "",
+	titleStyle = "",
+	inpType = "text",
+	inpPlaceholder = "Type here....",
+	inpValue,
+	onInpChange,
+	inpStyle = "",
+	isRequired = false,
+	min = "",
+	max = "",
+	autofocus = false,
+	autoComplete = "off",
+	name = "",
 }) => {
-  return (
-    <div className={`flex flex-col w-min ${mainBgStyle}`}>
-      <span className={`mx-2 mb-1 font-semibold ${titleStyle}`}>{title}</span>
-      <input
-        type={inpType}
-        placeholder={inpPlaceholder}
-        value={inpValue}
-        onChange={onInpChange}
-        className={`rounded-md mx-2 my-1 px-2 border-2 border-black bg-transparent outline-none ${inpStyle}`}
-        required={isRequired}
-        minLength={min}
-        maxLength={max}
-      />
-    </div>
-  )
+	// Opens the date picker on click
+	const handleClick = (e) => {
+		if (inpType === "date") {
+			e.target.showPicker()
+		}
+	}
+
+	return (
+		<div className={twMerge("flex flex-col w-min", mainBgStyle)}>
+			<span className={twMerge("mx-2 mb-1 font-semibold", titleStyle)}>
+				{title}
+			</span>
+			<input
+				name={name}
+				autoComplete={autoComplete}
+				autoFocus={autofocus}
+				type={inpType}
+				placeholder={inpPlaceholder}
+				value={inpValue}
+				onChange={onInpChange}
+				className={twMerge(
+					"rounded-md mx-2 my-1 px-2 bg-transparent outline-none",
+					inpStyle
+				)}
+				required={isRequired}
+				minLength={min}
+				maxLength={max}
+				onClick={handleClick}
+			/>
+		</div>
+	)
 }
 
 export default InputField
