fd9e80c0e260d145fe8a2a5e6ae999d454d9d445
RealSkCodes <codingwithrealsk@gmail.com>
2025-02-25 10:08:47 +0530

    chore: remove unused LLM.py file and some unwanted  imports from App.jsx

--- hire-when/frontend/src/App.jsx
+++ hire-when/frontend/src/App.jsx
@@ -1,20 +1,18 @@
 import { Outlet } from "react-router"
-import Header from "./components/Header.jsx"
 import Sidebar from "./components/Sidebar.jsx"
-import Body from "./components/Body.jsx"
 import LlmSidebar from "./components/LlmSidebar.jsx"
 
 function App() {
-  return (
-    <>
-      {/* <Header /> */}
-      <div className="flex w-full">
-        <Sidebar />
-        <Outlet />
-        <LlmSidebar />
-      </div>
-    </>
-  )
+	return (
+		<>
+			{/* <Header /> */}
+			<div className="flex w-full">
+				<Sidebar />
+				<Outlet />
+				<LlmSidebar />
+			</div>
+		</>
+	)
 }
 
 export default App
